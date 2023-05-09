7cc366e0b9f77db3d0928404b18ef3342953fdf9
nandatritona <nandhafebrian3@gmail.com>
2023-05-09 09:03:55 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+import Slider from "./components/slider/Slider";
+import Virtual from "./components/virtual/Virtual";
 
 function App() {
-  return (
-    <div className="App">
-      <span>Subscribe Zainkeepscode</span>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Hero/>
+            <Slider/>
+            <Virtual/>
+        </div>
+    );
 }
 
 export default App;
