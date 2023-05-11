b2c5b0f40278590942f738540a1f156e495fde64
nandatritona <nandhafebrian3@gmail.com>
2023-05-11 14:14:06 +0700

    add responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import css  from './Header.module.css';
 import Logo from '../../assets/logo.png';
 import {CgShoppingBag} from 'react-icons/cg';
+import {GoThreeBars} from 'react-icons/go';
 const Header = () => {
+
+    const [showMenu, setShowMenu] = useState(true);
+
+    const toggleMenu = () => {
+        setShowMenu((showMenu) => !showMenu);
+    }
     return (
         <div className={css.container}>
             <div className={css.logo}>
-                <img src={Logo} alt="brand-logo" />
+                <img src={Logo} alt="" />
                 <span>Shooping</span>
             </div>
 
             <div className={css.right}>
-                <div className={css.menu}>
-                    <ul className={css.menu}>
-                        <li>Collections</li>
-                        <li>Brands</li>
-                        <li>New</li>
-                        <li>Sales</li>
-                        <li>ENG</li>
-                    </ul>
+                <div className={css.bars} onClick={toggleMenu}>
+                    <GoThreeBars/>
                 </div>
+                {/* <div className={css.menu}></div> */}
+                <ul className={css.menu} style={{display: showMenu ? 'inherit' : 'none'}}>
+                    <li>Collections</li>
+                    <li>Brands</li>
+                    <li>New</li>
+                    <li>Sales</li>
+                    <li>ENG</li>
+                </ul>
 
                 <input type="text" className={css.search} placeholder='Seacrh'/>
 
