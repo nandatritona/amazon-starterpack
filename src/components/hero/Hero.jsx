3383cad0dcb9f204706c96d4ff9ba7279927fb4c
nandatritona <nandhafebrian3@gmail.com>
2023-05-11 08:48:06 +0700

    menambahkan frame motion

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,8 +3,12 @@ import css from './Hero.module.css';
 import HeroImg from '../../assets/hero.png';
 import {RiShoppingBagFill} from 'react-icons/ri';
 import {BsArrowRight} from 'react-icons/bs';
+import {motion} from "framer-motion";
 
 const Hero = () => {
+
+    const transition = {duration: 3, type: "spring"}
+
     return (
         <div className={css.container}>
             {/* Left side */}
@@ -19,19 +23,36 @@ const Hero = () => {
 
             {/* middle side hero image */}
             <div className={css.wrapper}>
-                <div className={css.blueCircle}></div>
-                <img src={HeroImg} alt="brand-model" width={600}/>
-                <div className={css.cart2}>
-                    <RiShoppingBagFill/>
 
+                {/* blue circle */}
+                <motion.div 
+                initial={{bottom: "2rem"}}
+                whileInView={{bottom: "0rem"}}
+                className={css.blueCircle}
+                transition={transition}
+                ></motion.div>
+
+                {/* hero image */}
+                <motion.img 
+                transition={transition}
+                initial={{bottom: "-2rem"}}
+                whileInView={{bottom: "0rem"}}
+                src={HeroImg} alt="brand-model" width={600}/>
+
+                {/* cart div animating sigup */}
+                <motion.div
+                transition={transition}
+                initial={{right: "4%"}}
+                whileInView={{right: "2%"}}
+                className={css.cart2}>
+                    <RiShoppingBagFill/>
                     <div className={css.signup}>
                         <span>Best Signup Offers</span>
-
                         <div>
                             <BsArrowRight/>
                         </div>
                     </div>
-                </div>
+                </motion.div>
             </div>
 
             {/* right side */}
